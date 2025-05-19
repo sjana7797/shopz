986787b08eba0ed83a7d664c63b29eee65a760e0
sjana7797 <sjana7797@gmail.com>
2025-05-19 20:34:51 +0530

    fix: adding synpack

--- packages/api/src/service.ts
+++ packages/api/src/service.ts
@@ -2,6 +2,7 @@ export const CLIENTS = {
   AUTH_SERVICE: "AUTH_SERVICE",
   MERCH_MANAGE_SERVICE: "MERCH_MANAGE_SERVICE",
   GATEWAY_SERVICE: "GATEWAY_SERVICE",
+  INSTA_SHOP_SERVICE: "INSTA_SHOP_SERVICE",
 } as const;
 
 export type CLientKey = keyof typeof CLIENTS;
@@ -9,9 +10,10 @@ export type CLientKey = keyof typeof CLIENTS;
 export type Client = (typeof CLIENTS)[CLientKey];
 
 export const PORT: Record<Client, number> = {
+  GATEWAY_SERVICE: 5000,
   AUTH_SERVICE: 5001,
   MERCH_MANAGE_SERVICE: 5002,
-  GATEWAY_SERVICE: 5000,
+  INSTA_SHOP_SERVICE: 5003,
 };
 
 export const getServiceURL = (client: Client) =>
---
+++ syncpack.config.ts
@@ -0,0 +1,8 @@
+export default {
+  semverGroups: [
+    {
+      isIgnored: true,
+      packages: ["@repo/**"],
+    },
+  ],
+} satisfies import("syncpack").RcFile;
